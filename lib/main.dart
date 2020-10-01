208f3a86cb40b96c9e1a16e32bd0f7b106d525cf
Ian Noble <ianoble@gmail.com>
2020-10-01 10:16:25 -0700

    Updates

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,14 @@
 import 'package:endeavor_stake_pools/screens/dashboard.dart';
 //import 'package:endeavor_stake_pools/screens/pool_details.dart';
 import 'package:flutter/material.dart';
+import 'package:flame_splash_screen/flame_splash_screen.dart';
+//import 'package:flutter/services.dart';
+
+const String intitialTitle = 'Endeavor';
 
 void main() {
   runApp(MyApp());
+  //SystemChrome.setEnabledSystemUIOverlays([]);
 }
 
 class MyApp extends StatelessWidget {
@@ -22,12 +27,73 @@ class MyApp extends StatelessWidget {
               //displayColor: Colors.red,
             ),
       ),
-      home: DashboardScreen(title: 'Endeavor'),
+      //home: DashboardScreen(title: intitialTitle),
+      home: SplashScreenGame(), //OtherScreen(),
       // routes: {
-      //   DashboardScreen.id: (context) => DashboardScreen(title: 'Endeavor'),
+      //   DashboardScreen.id: (context) => DashboardScreen(title: intitialTitle),
       //   PoolDetails.id: (context) => PoolDetails(title: ''),
       // },
       // initialRoute: DashboardScreen.id,
+      //debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class SplashScreenGame extends StatefulWidget {
+  @override
+  _SplashScreenGameState createState() => _SplashScreenGameState();
+}
+
+class _SplashScreenGameState extends State<SplashScreenGame> {
+  FlameSplashController controller;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = FlameSplashController(
+        fadeInDuration: Duration(seconds: 2),
+        fadeOutDuration: Duration(seconds: 1),
+        waitDuration: Duration(seconds: 1),
+        autoStart: true);
+  }
+
+  @override
+  void dispose() {
+    controller.dispose(); // dispose it when necessary
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: FlameSplashScreen(
+        // showBefore: (BuildContext context) {
+        //   return Text("Before the logo");
+        // },
+        // showAfter: (BuildContext context) {
+        //   return Text("After the logo");
+        // },
+        theme: FlameSplashTheme(
+          backgroundDecoration: const BoxDecoration(color: Color(0xFF000000)),
+          logoBuilder: (context) => Image(
+            width: 300,
+            image: const AssetImage(
+              'assets/flame-logo-black.gif',
+              package: 'flame_splash_screen',
+            ),
+          ),
+        ),
+        //onFinish: (context) => Navigator.pop(context),
+        onFinish: (BuildContext context) => Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => DashboardScreen(
+              title: intitialTitle,
+            ),
+          ),
+        ),
+        controller: controller,
+      ),
     );
   }
 }
